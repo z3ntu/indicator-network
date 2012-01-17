a5c39ac975aa576ce545809c8993545049e44124
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2012-01-17 14:31:02 -0300

    * Used the same property name from gdbusmenu  in QDebusMenuItem

--- client/dbusmodel/dbusmenuconst.h
+++ client/dbusmodel/dbusmenuconst.h
@@ -3,15 +3,4 @@
 
 #define DBUSMENU_INTERFACE                          "com.canonical.dbusmenu"
 
-#define DBUSMENU_PROPERTY_ID                        "dbusmenu_id"
-#define DBUSMENU_PROPERTY_LABEL                     "dbusmenu_label"
-#define DBUSMENU_PROPERTY_ICON_NAME                 "dbusmenu_icon_name"
-#define DBUSMENU_PROPERTY_STATE                     "dbusmenu_state"
-#define DBUSMENU_PROPERTY_HAS_SUBMENU               "dbusmenu_has_submenu"
-
-#define DBUSMENU_PROPERTY_GSETTINGS_SCHEMA          "x-gsettings-schema"
-#define DBUSMENU_PROPERTY_GSETTINGS_NAME            "x-gsettings-name"
-#define DBUSMENU_PROPERTY_GSETTINGS_TYPE            "x-gsettings-type"
-#define DBUSMENU_PROPERTY_GSETTINGS_DATA            "x-gsettings-data"
-
 #endif

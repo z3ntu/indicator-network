378d981ff6b02d1955e7434607d355f71f32b5a5
Alberto Ruiz <alberto.ruiz@canonical.com>
2012-01-23 18:06:08 +0000

    Fixed some inheritance problems in the NM secret agent

--- server/secret-agent-daemon.c
+++ server/secret-agent-daemon.c
@@ -3,5 +3,16 @@
 gint
 main (gint argc, gchar** argv)
 {
+  GMainLoop     *loop;
+  NMSecretAgent *agent;
+
+  g_type_init ();
+
+  agent = NM_SECRET_AGENT (unity_settings_secret_agent_new ());
+  nm_secret_agent_register (agent);
+
+  loop = g_main_loop_new (NULL, FALSE);
+  g_main_loop_run (loop);
+  g_main_loop_unref (loop);
   return 0;
 }

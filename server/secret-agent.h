92e6eea30f8bc49fb07f8dd7b1e93c71114a596e
Alberto Ruiz <alberto.ruiz@canonical.com>
2012-02-01 20:11:46 +0000

    Added more logic to handle multiple requests.

--- server/secret-agent.h
+++ server/secret-agent.h
@@ -18,17 +18,34 @@ G_BEGIN_DECLS
 #define UNITY_SETTINGS_IS_SECRET_AGENT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), UNITY_SETTINGS_TYPE_SECRET_AGENT))
 #define UNITY_SETTINGS_SECRET_AGENT_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), UNITY_SETTINGS_TYPE_SECRET_AGENT, UnitySettingsSecretAgentClass))
 
+#define UNITY_SETTINGS_SECRET_AGENT_SECRET_REQUESTED  "secret-requested"
+#define UNITY_SETTINGS_SECRET_AGENT_REQUEST_CANCELLED "request-cancelled"
+
 typedef struct _UnitySettingsSecretAgent UnitySettingsSecretAgent;
 typedef struct _UnitySettingsSecretAgentClass UnitySettingsSecretAgentClass;
 typedef struct _UnitySettingsSecretAgentPrivate UnitySettingsSecretAgentPrivate;
 
 struct _UnitySettingsSecretAgent {
-	NMSecretAgent parent_instance;
-	UnitySettingsSecretAgentPrivate * priv;
+  NMSecretAgent parent_instance;
+  UnitySettingsSecretAgentPrivate * priv;
 };
 
 struct _UnitySettingsSecretAgentClass {
-	NMSecretAgentClass parent_class;
+  NMSecretAgentClass parent_class;
+
+  void  (*secret_requested) (UnitySettingsSecretAgent      *self,
+                             guint64                        id,
+                             NMConnection                  *connection,
+                             const char                    *setting_name,
+                             const char                   **hints,
+                             NMSecretAgentGetSecretsFlags   flags);
+
+  void  (*requeste_cancelled) (UnitySettingsSecretAgent      *self,
+                               guint64                        id,
+                               NMConnection                  *connection,
+                               const char                    *setting_name,
+                               const char                   **hints,
+                               NMSecretAgentGetSecretsFlags   flags);
 };
 
 
@@ -36,6 +53,11 @@ GType                     unity_settings_secret_agent_get_type  (void) G_GNUC_CO
 UnitySettingsSecretAgent* unity_settings_secret_agent_new       (void);
 UnitySettingsSecretAgent* unity_settings_secret_agent_construct (GType object_type);
 
+void unity_settings_secret_agent_provide_secret (UnitySettingsSecretAgent *agent,
+                                                 guint64                   request,
+                                                 GHashTable               *secrets);
+void unity_settings_secret_agent_cancel_request (UnitySettingsSecretAgent *agent,
+                                                 guint64                   request);
 
 G_END_DECLS
 

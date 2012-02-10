7af3770a15ca30c464f64293f0d60a6299524c9a
Alberto Ruiz <alberto.ruiz@canonical.com>
2012-02-10 16:51:01 +0000

    Changed widget name from infobar to listitem

--- server/accesspoint-info.c
+++ server/accesspoint-info.c
@@ -2,6 +2,7 @@
 #include <nm-access-point.h>
 #include <nm-device.h>
 #include <nm-device-wifi.h>
+#include <nm-utils.h>
 #include <libdbusmenu-glib/menuitem.h>
 
 #include "accesspointitem.h"
@@ -9,15 +10,30 @@
 #define CONNECTED "Connected"
 #define DISCONNECTED "Disconnected"
 
-DbusmenuMenuitem*
+static DbusmenuMenuitem*
+create_infobar (gchar *label,
+                gchar *extra_label)
+{
+  DbusmenuMenuitem *item = dbusmenu_menuitem_new ();
+  dbusmenu_menuitem_property_set (item, DBUSMENU_MENUITEM_PROP_LABEL, label);
+  dbusmenu_menuitem_property_set(DBUSMENU_MENUITEM(item),
+                                 "type", "x-system-settings");
+  dbusmenu_menuitem_property_set(DBUSMENU_MENUITEM(item),
+                                 "x-tablet-widget", "unity.widgets.systemsettings.tablet.listitem");
+  dbusmenu_menuitem_property_set(DBUSMENU_MENUITEM(item),
+                                 "x-extra-label", extra_label);
+  return item;
+}
+
+
+static DbusmenuMenuitem*
 create_status_item (NMAccessPoint *ap, NMDevice *device)
 {
   NMAccessPoint *active;
-  DbusmenuMenuitem* item = dbusmenu_menuitem_new ();
   gchar *status = DISCONNECTED;
   gboolean conected = FALSE;
 
-  dbusmenu_menuitem_property_set (item, DBUSMENU_MENUITEM_PROP_LABEL, "Status");
+  /* TODO: Remove existing items in the submenu */
 
   g_object_get(device,
                "active-access-point", &active,
@@ -29,22 +45,39 @@ create_status_item (NMAccessPoint *ap, NMDevice *device)
   if (conected)
       status = CONNECTED;
 
-  dbusmenu_menuitem_property_set(DBUSMENU_MENUITEM(item),
-                                 "type", "x-system-settings");
-  dbusmenu_menuitem_property_set(DBUSMENU_MENUITEM(item),
-                                 "x-tablet-widget", "unity.widgets.systemsettings.tablet.infobar");
+  return create_infobar ("Status", status);
+}
 
-  dbusmenu_menuitem_property_set(DBUSMENU_MENUITEM(item),
-                                 "x-extra-label", status);
+static DbusmenuMenuitem*
+create_network_item (NMAccessPoint *ap)
+{
+  DbusmenuMenuitem *item;
+  gchar            *ssid = nm_utils_ssid_to_utf8 (nm_access_point_get_ssid (ap));
 
+  item = create_infobar ("Network", ssid);
+
+  g_free (ssid);
   return item;
+}
+
+static DbusmenuMenuitem*
+create_signalstrength_item ()
+{
+  return NULL;
 }
 
 void
 create_accespoint_submenu (DbusmenuAccesspointitem *item)
 {
-  DbusmenuMenuitem *status = create_status_item (dbusmenu_accesspointitem_get_ap (item),
-                                                 dbusmenu_accesspointitem_get_device (item));
+  DbusmenuMenuitem *status  = create_status_item   (dbusmenu_accesspointitem_get_ap (item),
+                                                    dbusmenu_accesspointitem_get_device (item));
+  DbusmenuMenuitem *network = create_network_item  (dbusmenu_accesspointitem_get_ap (item));
+  DbusmenuMenuitem *strengh = create_signalstrength_item (dbusmenu_accesspointitem_get_ap (item));
 
-  dbusmenu_menuitem_child_append (item, status);
+  dbusmenu_menuitem_child_append (DBUSMENU_MENUITEM (item), status);
+  dbusmenu_menuitem_child_append (DBUSMENU_MENUITEM (item), network);
+
+  g_object_unref (status);
+  g_object_unref (network);
+  g_object_unref (strengh);
 }

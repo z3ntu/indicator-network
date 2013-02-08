41b61421dec61a75f52570b6f3eea15b970e3326
Alberto Ruiz <alberto.ruiz@canonical.com>
2013-02-08 15:18:38 +0000

    general: Added copyright headers

--- test/network.py
+++ test/network.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
+# Copyright 2013 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#      Alberto Ruiz <alberto.ruizo@canonical.com>
+
 import unittest
 import dbus
 import dbusmock

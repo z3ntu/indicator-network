2a5b88c7763d280a8e497c6cdaffabd48d57fa60
Alberto Ruiz <alberto.ruiz@canonical.com>
2013-03-14 11:33:58 +0000

    Remove any references to the name chewie

--- autogen.sh
+++ autogen.sh
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License along 
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-PKG_NAME="chewie"
+PKG_NAME="indicator-network"
 
 which gnome-autogen.sh || {
 	echo "You need gnome-common from GNOME GIT"

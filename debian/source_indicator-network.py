1c9874d31170ca3f01593011741c7af22c36b399
Ted Gould <ted@gould.cx>
2013-04-23 22:40:27 -0500

    Wrong crash database

--- debian/source_indicator-network.py
+++ debian/source_indicator-network.py
@@ -5,6 +5,6 @@ from xdg.BaseDirectory import xdg_cache_home
 def add_info(report):
 	if not apport.packaging.is_distro_package(report['Package'].split()[0]):
 		report['ThirdParty'] = 'True'
-		report['CrashDB'] = 'indicator-network'
+		report['CrashDB'] = 'indicator_network'
 
 	attach_file_if_exists(report, os.path.join(xdg_cache_home, 'upstart', 'indicator-network.log'), 'indicator-network.log')

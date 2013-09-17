4acb5f01081ebacfa923bc457c3ec0ebe4b9eaa3
Pete Woods <pete.woods@canonical.com>
2013-09-17 17:54:50 +0100

    Copyright

--- secret-agent/gtk/PasswordMenu.h
+++ secret-agent/gtk/PasswordMenu.h
@@ -1,8 +1,19 @@
 /*
- * PasswordMenu.h
+ * Copyright (C) 2013 Canonical, Ltd.
  *
- *  Created on: 16 Sep 2013
- *      Author: pete
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Pete Woods <pete.woods@canonical.com>
  */
 
 #ifndef PASSWORDMENU_H_

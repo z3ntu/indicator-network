ec388a36d622656c6ef725fbbd31e9a2b7bc7ecf
Pete Woods <pete.woods@canonical.com>
2014-04-01 12:43:56 +0100

    Skeleton of test for SwitchItem

---
+++ tests/unit/network/menuitems/test-switch-item.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Pete Woods <pete.woods@canonical.com>
+ */
+
+#include <cassert>
+
+#include <menuitems/switch-item.h>
+
+#include <libqtdbustest/DBusTestRunner.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace testing;
+using namespace QtDBusTest;
+
+namespace
+{
+
+class TestSwitchItem : public Test
+{
+protected:
+    void
+    SetUp ()
+    {
+    }
+
+    DBusTestRunner dbus;
+};
+
+TEST_F(TestSwitchItem, ExportBasicActionsAndMenu)
+{
+    auto switchItem = make_shared<SwitchItem>("label", "prefix", "name");
+}
+
+} // namespace
